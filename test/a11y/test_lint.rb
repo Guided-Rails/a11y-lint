0e1a317b795bdddb1839702531a61e417fbe5f18
Abdullah Hashim <abdul@hey.com>
2026-03-18 15:56:38 -0400

    remove failing test

--- test/a11y/test_lint.rb
+++ test/a11y/test_lint.rb
@@ -6,8 +6,4 @@ class A11y::TestLint < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::A11y::Lint::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
