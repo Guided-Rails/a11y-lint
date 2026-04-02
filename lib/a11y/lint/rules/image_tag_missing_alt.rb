611e8abc007f17b7d2bf28fbd167d20b63cf914e
Abdullah Hashim <abdul@hey.com>
2026-04-02 07:51:11 -0400

    wip

--- lib/a11y/lint/rules/image_tag_missing_alt.rb
+++ lib/a11y/lint/rules/image_tag_missing_alt.rb
@@ -26,6 +26,8 @@ module A11y
           call && !alt_key_within?(call)
         end
 
+        # Walks the Ripper S-expression tree to find
+        # the image_tag call node, if present.
         def extract_image_tag_call(sexp)
           return unless sexp.is_a?(Array)
           return sexp if image_tag_call?(sexp)
@@ -38,6 +40,9 @@ module A11y
           nil
         end
 
+        # Matches both calling styles:
+        #   image_tag "photo.jpg"  => :command
+        #   image_tag("photo.jpg") => :method_add_arg
         def image_tag_call?(sexp)
           case sexp
           in [:command, [:@ident, "image_tag", *], *] then true
@@ -46,6 +51,8 @@ module A11y
           end
         end
 
+        # Recursively searches the sexp for an
+        # :assoc_new node whose key is "alt".
         def alt_key_within?(sexp)
           return true if alt_key?(sexp)
           return false unless sexp.is_a?(Array)
@@ -53,14 +60,29 @@ module A11y
           sexp.any? { |child| alt_key_within?(child) }
         end
 
+        # Checks if a sexp is a hash pair (assoc_new)
+        # with "alt" as the key.
         def alt_key?(sexp)
           return false unless sexp.is_a?(Array) && sexp[0] == :assoc_new
 
-          key = sexp[1]
-          (key in [:@label, "alt:", *]) ||
-            (key in [:string_literal,
-                     [:string_content,
-                      [:@tstring_content, "alt", *]]])
+          alt_key_value?(sexp[1])
+        end
+
+        def alt_key_value?(key)
+          alt_symbol_key?(key) || alt_string_key?(key)
+        end
+
+        # Matches symbol-style key: `alt: "..."`
+        def alt_symbol_key?(key)
+          key in [:@label, "alt:", *]
+        end
+
+        # Matches string-style key: `"alt" => "..."`
+        def alt_string_key?(key)
+          key in [
+            :string_literal,
+            [:string_content, [:@tstring_content, "alt", *]]
+          ]
         end
       end
     end
