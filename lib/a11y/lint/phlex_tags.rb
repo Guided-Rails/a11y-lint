7f6b2c7231429e4e0bd5cfaa7c780ae2124f1abc
Abdullah Hashim <abdul@hey.com>
2026-04-15 08:49:29 -0400

    wip

--- lib/a11y/lint/phlex_tags.rb
+++ lib/a11y/lint/phlex_tags.rb
@@ -12,20 +12,111 @@ module A11y
 
       HTML_TAGS = Set.new(
         %w[
-          a abbr address article aside b bdi bdo
-          blockquote body br button caption cite code
-          col colgroup data datalist dd del details dfn
-          dialog div dl dt em embed fieldset figcaption
-          figure footer form h1 h2 h3 h4 h5 h6 head
-          header hgroup hr html i iframe img input ins
-          kbd label legend li link main map mark menu
-          meter nav noscript object ol optgroup option
-          output p picture pre progress q rp rt
-          ruby_element s samp script search section
-          select slot small span strong style sub
-          summary sup table tbody td template_tag
-          textarea tfoot th thead time title tr u ul
-          var video wbr
+          a
+          abbr
+          address
+          article
+          aside
+          b
+          bdi
+          bdo
+          blockquote
+          body
+          br
+          button
+          caption
+          cite
+          code
+          col
+          colgroup
+          data
+          datalist
+          dd
+          del
+          details
+          dfn
+          dialog
+          div
+          dl
+          dt
+          em
+          embed
+          fieldset
+          figcaption
+          figure
+          footer
+          form
+          h1
+          h2
+          h3
+          h4
+          h5
+          h6
+          head
+          header
+          hgroup
+          hr
+          html
+          i
+          iframe
+          img
+          input
+          ins
+          kbd
+          label
+          legend
+          li
+          link
+          main
+          map
+          mark
+          menu
+          meter
+          nav
+          noscript
+          object
+          ol
+          optgroup
+          option
+          output
+          p
+          picture
+          pre
+          progress
+          q
+          rp
+          rt
+          ruby_element
+          s
+          samp
+          script
+          search
+          section
+          select
+          slot
+          small
+          span
+          strong
+          style
+          sub
+          summary
+          sup
+          table
+          tbody
+          td
+          template_tag
+          textarea
+          tfoot
+          th
+          thead
+          time
+          title
+          tr
+          u
+          ul
+          var
+          video
+          wbr
         ]
       ).freeze
 
