aea3bbd0dcb1edbff0fbc06588ad0ab8351a9fe0
Abdullah Hashim <abdul@hey.com>
2026-04-15 10:03:06 -0400

    wip

--- test/a11y/lint/test_erb_node.rb
+++ test/a11y/lint/test_erb_node.rb
@@ -9,59 +9,77 @@ module A11y
       def test_line
         node = ErbNode.new(nokogiri_node: make_element("img"), line: 5)
 
-        assert_equal(5, node.line)
+        result = node.line
+
+        assert_equal(5, result)
       end
 
       def test_tag_name_for_html_element
         node = ErbNode.new(nokogiri_node: make_element("img"), line: 1)
 
-        assert_equal("img", node.tag_name)
+        result = node.tag_name
+
+        assert_equal("img", result)
       end
 
       def test_tag_name_returns_nil_for_ruby_code_node
         node = ErbNode.new(ruby_code: "image_tag \"photo.jpg\"", line: 1)
 
-        assert_nil(node.tag_name)
+        result = node.tag_name
+
+        assert_nil(result)
       end
 
       def test_attribute_when_present
         element = make_element("img", "src" => "photo.jpg", "alt" => "A photo")
         node = ErbNode.new(nokogiri_node: element, line: 1)
 
-        assert(node.attribute?("alt"))
+        result = node.attribute?("alt")
+
+        assert(result)
       end
 
       def test_attribute_when_absent
         element = make_element("img", "src" => "photo.jpg")
         node = ErbNode.new(nokogiri_node: element, line: 1)
 
-        refute(node.attribute?("alt"))
+        result = node.attribute?("alt")
+
+        refute(result)
       end
 
       def test_attributes_with_multiple_attrs
         element = make_element("img", "src" => "photo.jpg", "alt" => "A photo")
         node = ErbNode.new(nokogiri_node: element, line: 1)
 
-        assert_equal({ "src" => true, "alt" => true }, node.attributes)
+        result = node.attributes
+
+        assert_equal({ "src" => true, "alt" => true }, result)
       end
 
       def test_attributes_returns_empty_hash_for_ruby_code_node
         node = ErbNode.new(ruby_code: "image_tag \"photo.jpg\"", line: 1)
 
-        assert_equal({}, node.attributes)
+        result = node.attributes
+
+        assert_equal({}, result)
       end
 
       def test_ruby_code_for_erb_output_node
         code = "image_tag \"photo.jpg\""
         node = ErbNode.new(ruby_code: code, line: 1)
 
-        assert_equal(code, node.ruby_code)
+        result = node.ruby_code
+
+        assert_equal(code, result)
       end
 
       def test_ruby_code_returns_nil_for_html_element_node
         node = ErbNode.new(nokogiri_node: make_element("img"), line: 1)
 
-        assert_nil(node.ruby_code)
+        result = node.ruby_code
+
+        assert_nil(result)
       end
 
       def test_children_returns_direct_element_children
@@ -71,7 +89,9 @@ module A11y
         ul = doc.at_css("ul")
         node = ErbNode.new(nokogiri_node: ul, line: ul.line)
 
-        assert_equal(%w[li li], node.children.map(&:tag_name))
+        result = node.children.map(&:tag_name)
+
+        assert_equal(%w[li li], result)
       end
 
       def test_children_excludes_text_nodes
@@ -81,42 +101,55 @@ module A11y
         ul = doc.at_css("ul")
         node = ErbNode.new(nokogiri_node: ul, line: ul.line)
 
-        assert_equal(["li"], node.children.map(&:tag_name))
+        result = node.children.map(&:tag_name)
+
+        assert_equal(["li"], result)
       end
 
       def test_children_returns_empty_for_ruby_code_node
         node = ErbNode.new(ruby_code: "render \"items\"", line: 1)
 
-        assert_empty(node.children)
+        result = node.children
+
+        assert_empty(result)
       end
 
       def test_call_node_for_erb_output_node
         node = ErbNode.new(ruby_code: 'image_tag("photo.jpg")', line: 1)
 
-        assert_instance_of(Prism::CallNode, node.call_node)
-        assert_equal("image_tag", node.call_node.name.to_s)
+        result = node.call_node
+
+        assert_instance_of(Prism::CallNode, result)
+        assert_equal("image_tag", result.name.to_s)
       end
 
       def test_call_node_returns_nil_for_html_element_node
         node = ErbNode.new(nokogiri_node: make_element("img"), line: 1)
 
-        assert_nil(node.call_node)
+        result = node.call_node
+
+        assert_nil(result)
       end
 
       def test_call_node_with_block_form
         node = ErbNode.new(ruby_code: 'link_to("#") do', line: 1)
 
-        assert_instance_of(Prism::CallNode, node.call_node)
-        assert_equal("link_to", node.call_node.name.to_s)
-        refute_nil(node.call_node.block)
+        result = node.call_node
+
+        assert_instance_of(Prism::CallNode, result)
+        assert_equal("link_to", result.name.to_s)
+
+        refute_nil(result.block)
       end
 
       def test_call_node_with_multiline_code
         code = "link_to(\"#\",\n            class: \"icon\")"
         node = ErbNode.new(ruby_code: code, line: 1)
 
-        assert_instance_of(Prism::CallNode, node.call_node)
-        assert_equal("link_to", node.call_node.name.to_s)
+        result = node.call_node
+
+        assert_instance_of(Prism::CallNode, result)
+        assert_equal("link_to", result.name.to_s)
       end
 
       private
